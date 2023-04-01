31e8c4b81fd83ff7494e88de3701534624128419
Jack <jakitmationstudios@gmail.com>
2023-04-01 02:16:01 -0400

    No auction :( (yet)

--- ui/auction.py
+++
@@ -1,9 +0,0 @@
-import nextcord
-from nextcord import Interaction
-from nextcord import ui
-
-
-# TODO: This.
-class AuctionView(ui.View):
-    def __init__(self) -> None:
-        super().__init__()

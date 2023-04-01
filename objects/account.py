1cbf1b8587d28fc619204766ec8711355e351dad
Jack <jakitmationstudios@gmail.com>
2023-04-01 13:31:36 -0400

    Removed dev stash

--- objects/account.py
+++ objects/account.py
@@ -14,42 +14,14 @@ class Account:
     in_menu: bool
     menu: View
     
-    # TODO: Remove OP starter stash.
     def __init__(self, user: nextcord.User) -> None:
         self.user = user.id
         
         self.stash = Storage("Stash", 50)
         
-        self.stash.insert_item(trinkets.AncientCoin())
-        
-        for _ in range(2):
-            for _ in range(2):
-                self.stash.insert_item(alloys.BronzeAlloy())
-                self.stash.insert_item(alloys.CrystalAlloy())
-                self.stash.insert_item(alloys.ExoticAlloy())
-                self.stash.insert_item(alloys.IronAlloy())
-                self.stash.insert_item(alloys.MysticAlloy())
-                self.stash.insert_item(alloys.PlatinumAlloy())
-                self.stash.insert_item(alloys.SteelAlloy())
-                self.stash.insert_item(alloys.TitaniumAlloy())
-            
-            self.stash.insert_item(weapons.StandardBow())
-            self.stash.insert_item(weapons.StandardSword())
-            self.stash.insert_item(weapons.StandardDagger())
-            self.stash.insert_item(weapons.StandardShield())
-        
-            self.stash.insert_item(armors.StandardHeadband())
-            self.stash.insert_item(armors.StandardSkates())
-            self.stash.insert_item(armors.StandardHelmet())
-            self.stash.insert_item(armors.StandardRobe())
-            self.stash.insert_item(armors.StandardScarf())
-            self.stash.insert_item(armors.StandardShoes())
-            self.stash.insert_item(armors.StandardSkull())
-            self.stash.insert_item(armors.StandardTunic())
-        
         self.player = DisCharacter(10, 0, 0, 0, 5, self.user)
         
-        self.money = 1_000_000
+        self.money = 0
         
         self.in_dungeon = False
         self.in_menu = False

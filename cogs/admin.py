0c1c6aa035159711e7a679342d45cd5649a95899
Jack <jakitmationstudios@gmail.com>
2023-04-01 13:52:31 -0400

    Restart admin command

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,6 +2,7 @@ import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
 import data
+import os, sys
 
 ADMIN_GUILDS = [658882526470864896]
 
@@ -14,6 +15,12 @@ class AdminCommands(commands.Cog):
     async def shutdown(self, interaction: Interaction):
         await interaction.send("Shutting down.", ephemeral=True)
         await self.client.close()
+        
+    @nextcord.slash_command(name="restart", description="Restarts the bot.", guild_ids=ADMIN_GUILDS)
+    async def shutdown(self, interaction: Interaction):
+        await interaction.send("Shutting down.", ephemeral=True)
+        await self.client.close()
+        os.execv(sys.argv[0], sys.argv)
 
     @nextcord.slash_command(name="wipe", description="Wipes all data", guild_ids=ADMIN_GUILDS)
     async def wipe(self, interaction: Interaction):
